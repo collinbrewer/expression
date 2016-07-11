d6d112cb33b4164b1ebc250b27e675902678a722
Collin Brewer <collin@brewer-collective.com>
2016-07-11 11:15:21 -0500

    Updated license

--- index.js
+++ index.js
@@ -2,6 +2,4 @@
 var Expression=require("./src/expression.js");
 
 // expose
-(function(mod, name){
-   (typeof(module)!=="undefined" ? (module.exports=mod) : ((typeof(define)!=="undefined" && define.amd) ? define(function(){ return mod; }) : (window[name]=mod)));
-})(Expression, "Expression");
+module.exports=Expression;
